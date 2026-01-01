79d05a90803f6fdb4b059bcad8a45e3ad1c6acc0
Ammon Shepherd <ammon.shepherd@gmail.com>
2026-01-01 15:54:52 -0500

    Build A Real About Me

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
  * Routes
  */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
-});
-
-app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/home.html'));
 })
 
