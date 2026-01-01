a9a3872dbd8b02a4e2f7be01d245befa59c9e6a7
Ammon Shepherd <ammon.shepherd@gmail.com>
2026-01-01 15:39:14 -0500

    Serve Static Files

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-// Import express using ESM syntax
+/**
+ * Imports
+ */
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// Create an instance of an Express application
-const app = express();
-
+/**
+ * Variables
+ */
 const name = process.env.NAME;
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Define a route handler for the root URL ('/')
+// Setup the Express Server
+const app = express();
+/**
+
+ * Express Middleware
+ */
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
     res.send(`Hello, ${name}!`);
 });
 
-// Define the prot number the server will listen on
-const PORT = 3000;
-
-// Start the server and listen on the specified port
+/**
+ * Start server on specified port
+ */
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 })
